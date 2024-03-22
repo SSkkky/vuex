d9eb3eeb3c9dd7ed8853d657c32ac25989a2a3c0
SSkkky <worte5633@gmail.com>
2024-03-22 10:23:35 +0900

    프로젝트 생성

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link :to="{name :'about', query:{num:1000}}">About</router-link>
+    </nav>
+    <router-view/>
+  </div>
 </template>
 
 <style lang="scss">
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,42 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>{{ $route.query.num }}</p>
+    
+    <hr/>
+    <div>
+      <form @submit.prevent="saveData">
+        <!-- <input type="text" name="id" v-model="text"/> -->
+        <input type="text" name="id" :value="text" @input="text=$event.target.value"/>
+        <button>저장</button>
+      </form>
+    </div>
   </div>
 </template>
+
+
+<script>
+  export default {
+    data(){
+      return{text:'농담곰'}
+    },
+    methods:{
+      saveData(){
+        console.log(this.text)
+      }
+    },
+    // updated(){
+    //   console.log(this.text)
+    // }
+  }
+</script>
+
+<style lang="scss" scoped>
+.about{
+  p{
+    font-weight: 700;
+    font-style: italic;
+    color: orangered;
+  }
+}
+</style>
